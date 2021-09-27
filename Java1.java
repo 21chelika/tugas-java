01c0ffcccb8f4456106c2a1fa31303a8627258b9
21chelika <85401286+21chelika@users.noreply.github.com>
2021-09-27 11:35:21 +0700

    Add files via upload

---
+++ Java1.java
@@ -0,0 +1,15 @@
+package punyachelika;
+
+/**
+ *
+ * @author Chelika Patricia
+ */
+public class Java1 {
+    public static void main(String[] args) {
+        
+         System.out.println("Belajar JAVA");
+        System.out.println("Sangat Mudah Sekali");
+        System.out.println("Dan Sangat Menyenangkan");
+        
+    }   
+}
---
+++ Java2.java
@@ -0,0 +1,16 @@
+package punyachelika;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Chelika Patricia
+ */
+public class Java2 {
+     public static void main(String[] args){
+         
+        String namaTeman = JOptionPane.showInputDialog("Anda sedang belajar apa?");
+        JOptionPane.showMessageDialog(null, "Belajar "+namaTeman+" Sangat mudah");
+        
+    }
+}
